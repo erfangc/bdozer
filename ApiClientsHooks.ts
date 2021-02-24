31de90716445693d0b78f60e58030e2ee2d2f1f4
erfangchen <eukHGfhS68hvfx>
2021-02-24 17:59:38 -0500

    wip

--- ApiClientsHooks.ts
+++ ApiClientsHooks.ts
@@ -1,19 +1,10 @@
 import { ModelBuilderControllerApi, ModelsControllerApi } from "./client";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { serverErrorStore } from "./ServerErrorStore";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || undefined;
-
-export function useModelsApi() {
-  const axiosInstance = useAxios();
-  return new ModelsControllerApi(null, basePath, axiosInstance);
-}
-
-export function useModelBuilderApi() {
-  const axiosInstance = useAxios();
-  return new ModelBuilderControllerApi(null, basePath, axiosInstance);
-}
 
 const axiosInstance = axios.create();
 
@@ -31,7 +22,23 @@ function useAxios() {
         },
       };
     });
+    axiosInstance.interceptors.response.use(null, (error) => {
+      console.log("... error here ...");
+
+      serverErrorStore.addError(error?.response?.data);
+      return Promise.reject(error);
+    });
   }, []);
 
   return axiosInstance;
+}
+
+export function useModelsApi() {
+  const axiosInstance = useAxios();
+  return new ModelsControllerApi(null, basePath, axiosInstance);
+}
+
+export function useModelBuilderApi() {
+  const axiosInstance = useAxios();
+  return new ModelBuilderControllerApi(null, basePath, axiosInstance);
 }
