c272469c1f1d78a0e32a08ed6f58543012512dd7
Erfang Chen <erfangc@gmail.com>
2021-03-25 16:31:41 -0400

    wip

--- components/Mvp/Narrative2/subcomponents/Overview.tsx
+++ components/Mvp/Narrative2/subcomponents/Overview.tsx
@@ -18,24 +18,22 @@ export function Overview(props: Props) {
     } = props;
     const upside = (targetPrice / currentPrice - 1) * 100;
     return (
-        <div className="flex-col space-y-8" id="overview">
-            <div className="flex justify-between items-center">
-                <div className="flex-col flex space-y-2">
-                    <span>{name}</span>
-                    <div className="flex items-baseline">
-                        <Title>{symbol}</Title>
-                        <span className="ml-4">Latest Price <span className={currentPrice < targetPrice ? `text-rose-500` : null}>${currentPrice.toFixed(2)}</span></span>
-                    </div>
+        <div className="flex justify-between items-center">
+            <div className="flex-col flex space-y-2">
+                <span>{name}</span>
+                <div className="flex items-baseline">
+                    <Title>{symbol}</Title>
+                    <span className="ml-4">Latest Price <span className={currentPrice < targetPrice ? `text-rose-500` : null}>${currentPrice.toFixed(2)}</span></span>
                 </div>
-                <div className="flex justify-between shadow-lg px-4 py-2 bg-blueGray-700 rounded-md">
-                    <div className="flex flex-col">
-                        <span className="font-semibold text-lg">Target Price</span>
-                        <div>
-                            <NumberFormat className="font-light" value={targetPrice} displayType="text" prefix="$" decimalScale={2} />
-                            <span className={`text-xs ml-3 ${upside > 0 ? 'text-lime-400' : 'bg-rose-500'} font-bold`}>
-                                {upside.toFixed(1)}% <span className="font-normal">Upside</span>
-                            </span>
-                        </div>
+            </div>
+            <div className="flex justify-between shadow-lg px-4 py-2 bg-blueGray-700 rounded-md">
+                <div className="flex flex-col">
+                    <span className="font-semibold text-lg">Target Price</span>
+                    <div>
+                        <NumberFormat className="font-light" value={targetPrice} displayType="text" prefix="$" decimalScale={2} />
+                        <span className={`text-xs ml-3 ${upside > 0 ? 'text-lime-400' : 'bg-rose-500'} font-bold`}>
+                            {upside.toFixed(1)}% <span className="font-normal">Upside</span>
+                        </span>
                     </div>
                 </div>
             </div>
