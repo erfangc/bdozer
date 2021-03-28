c6a0cf696b37729e5e5a1bd1aab31bb5b4624ef3
Erfang Chen <erfangc@gmail.com>
2021-03-28 13:23:17 -0400

    wip

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -15,7 +15,7 @@ function SettingsComponent() {
 
     return (
         <main className="text-blueGray-50 flex-grow flex flex-col space-y-6 justify-center items-center min-h-screen p-2 xl:p-10 lg:p-8">
-            <Title>Model Controls</Title>
+            <Title>Model Controls and Settings</Title>
             <FilingEntitySearch onSubmit={switchFilingEntity} className="w-80 lg:w-96" />
         </main>
     )
