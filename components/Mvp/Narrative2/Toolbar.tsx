1ab6d81dad143478782c097f5598d5045ff18776
Erfang Chen <erfangc@gmail.com>
2021-03-30 17:12:49 -0400

    wip

--- components/Mvp/Narrative2/Toolbar.tsx
+++ components/Mvp/Narrative2/Toolbar.tsx
@@ -13,8 +13,18 @@ export function Toolbar() {
         router.push('/request-stock')
     }
 
+    function browse() {
+        router.push('/search')
+    }
+
     return (
         <div className="shadow-md bg-blueGray-800 px-2 py-2 flex space-x-6 fixed bottom-0 lg:top-0 lg:bottom-auto left-0 lg:left-20 right-0 justify-center z-10">
+            <ToolButton onClick={browse} className="lg:hidden">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 13H5V11H3V13ZM3 17H5V15H3V17ZM3 9H5V7H3V9ZM7 13H21V11H7V13ZM7 17H21V15H7V17ZM7 7V9H21V7H7ZM3 13H5V11H3V13ZM3 17H5V15H3V17ZM3 9H5V7H3V9ZM7 13H21V11H7V13ZM7 17H21V15H7V17ZM7 7V9H21V7H7Z" fill="#CBD5E1" />
+                </svg>
+                Browse More
+            </ToolButton>
             <ToolButton onClick={requestEarlyAccess}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 17V19H9V17H3ZM3 5V7H13V5H3ZM13 21V19H21V17H13V15H11V21H13ZM7 9V11H3V13H7V15H9V9H7ZM21 13V11H11V13H21ZM15 9H17V7H21V5H17V3H15V9Z" fill="#CBD5E1" />
