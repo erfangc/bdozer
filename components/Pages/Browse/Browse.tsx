bfdb0622003c321468a9d8afe702e097dfc30369
Erfang Chen <erfangc@gmail.com>
2021-03-31 10:44:59 -0400

    wip

--- components/Pages/Browse/Browse.tsx
+++ components/Pages/Browse/Browse.tsx
@@ -20,22 +20,36 @@ export function Browse(props: Props) {
             <section>
                 <StockAnalysisSearch onSubmit={({ cik }) => navigate(cik)} className="mb-20 mt-16" />
                 <div className="mb-8">
-                    <h1 className="border-blueGray-700">
-                        <span className="bg-blue-500 px-2 py-1 rounded font-extrabold uppercase">
-                            #Featured stocks
-                        </span>
-                    </h1>
-                    <blockquote className="mt-8 pl-6 border-l-4 bg-blueGray-800 py-2 text-sm text-blueGray-300">
-                        Click on the Cards to View Analysis
-                    </blockquote>
+                    <div className="flex space-x-4">
+                        <StockTab active>#featured stocks</StockTab>
+                        {/* <StockTab >#FAANG</StockTab> */}
+                    </div>
                 </div>
                 <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mb-8">
                     {
                         stockAnalyses.map(stockAnalysis => <StockAnalysisCard stockAnalysis={stockAnalysis} />)
                     }
                 </div>
+                <blockquote className="mt-8 pl-6 border-l-4 bg-blueGray-800 py-2 text-sm text-blueGray-300">
+                    Click on the Cards to View Analysis
+                </blockquote>
             </section>
             <LegalDisclaimer />
         </main>
+    )
+}
+
+interface StockTabProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    active?: boolean
+}
+
+function StockTab({ active, className, children, ...props }: StockTabProps) {
+    return (
+        <button
+            className={`focus:outline-none px-2 py-1 rounded font-extrabold uppercase transition ${active ? 'bg-blue-500 cursor-default' : 'border-blueGray-500 bg-blueGray-700 shadow-lg hover:bg-blue-500 hover:border-0'} ease-linear ${className}`}
+            {...props}
+        >
+            {children}
+        </button>
     )
 }
