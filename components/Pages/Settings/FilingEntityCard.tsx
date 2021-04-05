ebad081139bf31f8baf33e770f177fdd8999c92b
Erfang Chen <erfangc@gmail.com>
2021-04-05 16:26:49 -0400

    wip

--- components/Pages/Settings/FilingEntityCard.tsx
+++ components/Pages/Settings/FilingEntityCard.tsx
@@ -3,29 +3,23 @@ import { FilingEntity } from "../../../client";
 export function FilingEntityCard({ filingEntity }: { filingEntity?: FilingEntity }) {
     if (!filingEntity) return null;
     return (
-        <>
-            <p className="self-end text-sm hidden lg:block">
-                <div className="font-light text-blueGray-300">Last Updated</div>
-                <div className="text-blueGray-200">{new Date(filingEntity.lastUpdated).toLocaleString()}</div>
-            </p>
-            <div className='bg-blueGray-700 shadow-md rounded-md grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-6 px-4 gap-4'>
-                <div className="flex flex-col">
-                    <div className="font-light text-sm">Name</div>
-                    <span className="font-semibold">{filingEntity?.name}</span>
-                </div>
-                <div className="flex flex-col">
-                    <div className="font-light text-sm">Trading Symbol</div>
-                    <span className="font-semibold">{filingEntity?.tradingSymbol}</span>
-                </div>
-                <div className="flex flex-col">
-                    <div className="font-light text-sm">SIC Description</div>
-                    <span className="font-semibold">{filingEntity?.sicDescription}</span>
-                </div>
-                <div className="flex flex-col">
-                    <div className="font-light text-sm">Exchange</div>
-                    <span className="font-semibold space-x-1">{filingEntity?.exchanges.map(exchange => <span>{exchange}</span>)}</span>
-                </div>
+        <div className='bg-blueGray-700 shadow-md rounded-md grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-6 px-4 gap-4'>
+            <div className="flex flex-col">
+                <div className="font-light text-sm">Name</div>
+                <span className="font-semibold">{filingEntity?.name}</span>
             </div>
-        </>
+            <div className="flex flex-col">
+                <div className="font-light text-sm">Trading Symbol</div>
+                <span className="font-semibold">{filingEntity?.tradingSymbol}</span>
+            </div>
+            <div className="flex flex-col">
+                <div className="font-light text-sm">SIC Description</div>
+                <span className="font-semibold">{filingEntity?.sicDescription}</span>
+            </div>
+            <div className="flex flex-col">
+                <div className="font-light text-sm">Exchange</div>
+                <span className="font-semibold space-x-1">{filingEntity?.exchanges.map(exchange => <span>{exchange}</span>)}</span>
+            </div>
+        </div>
     )
 }
