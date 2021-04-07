bb5a9c83ff049c403919b4d0513338b9bf4cb8fa
Erfang Chen <erfangc@gmail.com>
2021-04-07 15:54:45 -0400

    wip

--- components/Common/Card.tsx
+++ components/Common/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
     running?: boolean
 }
 
-export function Card(props: CardProps) {
+export function Money(props: CardProps) {
     const { running } = props;
     return (
         <div className="py-4 px-8 rounded-lg shadow-md bg-blueGray-700 flex-col flex space-y-2">
@@ -31,6 +31,30 @@ export function Card(props: CardProps) {
         </div>
     )
 }
+
+export function Number(props: CardProps) {
+    const { running } = props;
+    return (
+        <div className="py-4 px-8 rounded-lg shadow-md bg-blueGray-700 flex-col flex space-y-2">
+            <span className="text-xs">{props.label}</span>
+            {
+                running
+                    ?
+                    <div
+                        className="h-8 animate-pulse bg-blueGray-500 rounded"
+                    >
+                    </div>
+                    : <NumberFormat
+                        displayType='text'
+                        className="text-2xl"
+                        value={props.value}
+                        decimalScale={2}
+                    />
+            }
+        </div>
+    )
+}
+
 export function CardPercent(props: CardProps) {
     const { running } = props;
     return (
--- components/Pages/ControlPanel/StockAnalysisSummary.tsx
+++ components/Pages/ControlPanel/StockAnalysisSummary.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import { StockAnalysis2 } from '../../../client'
-import { Card, CardPercent } from '../../Common/Card'
+import { Money, CardPercent, Number } from '../../Common/Card'
 
 interface Props {
     stockAnalysis: StockAnalysis2
@@ -17,9 +17,11 @@ export default function AnalysisSummary(props: Props) {
         ?
         <div>
             <div className="grid grid-flow-row gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-4">
-                <Card value={stockAnalysis?.derivedStockAnalytics?.currentPrice} label={"Current Price"} running={loading} />
-                <Card value={stockAnalysis?.derivedStockAnalytics?.targetPrice} label={"Target Price"} running={loading} />
-                <Card value={stockAnalysis?.derivedStockAnalytics?.zeroGrowthPrice} label={"Zero Growth Price"} running={loading} />
+                <Money value={stockAnalysis?.derivedStockAnalytics?.currentPrice} label={"Current Price"} running={loading} />
+                <Money value={stockAnalysis?.derivedStockAnalytics?.targetPrice} label={"Target Price"} running={loading} />
+                <Money value={stockAnalysis?.derivedStockAnalytics?.zeroGrowthPrice} label={"Zero Growth Price"} running={loading} />
+                <Number value={stockAnalysis?.model?.beta} label={"Beta"} running={loading} />
+                <CardPercent value={stockAnalysis?.derivedStockAnalytics?.discountRate} label={"Discount Rate"} running={loading} />
                 <CardPercent value={stockAnalysis?.derivedStockAnalytics?.revenueCAGR} label={"Revenue CAGR"} running={loading} />
             </div>
         </div>
