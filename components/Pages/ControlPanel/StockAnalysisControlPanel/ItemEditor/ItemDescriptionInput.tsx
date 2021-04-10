1b1f7b3822be848f861f6622f4e6c3c353ed7c06
Erfang Chen <erfangc@gmail.com>
2021-04-10 14:29:31 -0400

    wip

--- components/Pages/ControlPanel/StockAnalysisControlPanel/ItemEditor/ItemDescriptionInput.tsx
+++ components/Pages/ControlPanel/StockAnalysisControlPanel/ItemEditor/ItemDescriptionInput.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { Item } from "../../../../../client";
-import { TextInput } from "../../../../Common/TextInput";
+import {useState} from "react";
+import {Item} from "../../../../../client";
+import {TextArea} from "../../../../Common/Textarea";
 
 export function ItemDescriptionInput(props: { item: Item, onChange: (newValue) => void }) {
-    const { item, onChange } = props;
+    const {item, onChange} = props;
     const [value, setValue] = useState(item.description)
     return (
-        <TextInput
+        <TextArea
             label="Description"
             value={value}
             className="min-w-full"
-            onChange={({ currentTarget: { value } }) => setValue(value)}
+            onChange={({currentTarget: {value}}) => setValue(value)}
             onBlur={() => onChange(value)}
             placeholder="ex: Commercial Aircraft"
         />
