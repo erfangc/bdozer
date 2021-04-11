fb05bd871d0d945704ce7b217fc46e78091189e6
Erfang Chen <erfangc@gmail.com>
2021-04-10 21:48:36 -0400

    wip

--- components/Pages/ControlPanel/StockAnalysisControlPanel/ItemDisplay.tsx
+++ components/Pages/ControlPanel/StockAnalysisControlPanel/ItemDisplay.tsx
@@ -1,12 +1,12 @@
-import {useRouter} from "next/router"
+import { useRouter } from "next/router"
 import React from "react"
 import NumberFormat from "react-number-format"
-import {Item, StockAnalysis2} from "../../../../client"
-import {Attention as Manual, Check} from "./ItemEditor/Svgs"
-import {History} from "./History";
-import {useFactBaseUnsecured} from "../../../../api-hooks";
-import {SecondaryButton, SmallSecondaryButton} from "../../../Common/SecondaryButton";
-import {SmallGhostButton} from "../../../Common/GhostButton";
+import { Item, StockAnalysis2 } from "../../../../client"
+import { Attention as Manual, Check, Nothing } from "./ItemEditor/Svgs"
+import { History } from "./History";
+import { useFactBaseUnsecured } from "../../../../api-hooks";
+import { SecondaryButton, SmallSecondaryButton } from "../../../Common/SecondaryButton";
+import { SmallGhostButton } from "../../../Common/GhostButton";
 
 interface Props {
     overriden?: boolean
@@ -16,26 +16,26 @@ interface Props {
 
 export function ItemDisplay(props: Props) {
 
-    const {item, overriden} = props
+    const { item, overriden } = props
     const router = useRouter()
-    const {id} = router.query
+    const { id } = router.query
     const factBase = useFactBaseUnsecured()
 
     async function openSourceDocument(event: React.MouseEvent<HTMLButtonElement>) {
         event.preventDefault()
         event.stopPropagation()
         const factId = item.historicalValue?.factId
-        const {data: fact} = await factBase.getFact(factId)
+        const { data: fact } = await factBase.getFact(factId)
         window.open(fact.sourceDocument)
     }
 
     return (
         <div
-            className="cursor-pointer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-md py-2  bg-blueGray-800 shadow-lg px-4 hover:bg-blueGray-700 transition ease-linear"
+            className={`cursor-pointer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-md py-2   ${item.subtotal ? '' : 'bg-blueGray-800'} shadow-lg px-4 hover:bg-blueGray-700 transition ease-linear`}
             onClick={() => router.push(`/control-panel/stock-analyses/${id}/items/${item.name}`)}
         >
             <div className="flex items-center">
-                <span>{item.description ?? item.name}</span>
+                <span className={item.subtotal ? 'font-extrabold' : ''}>{item.description ?? item.name}</span>
             </div>
             <div className="flex justify-start md:justify-end items-center">
                 <NumberFormat
@@ -47,8 +47,8 @@ export function ItemDisplay(props: Props) {
             </div>
             <div className="flex justify-start lg:justify-end items-center space-x-4">
                 <SmallGhostButton onClick={openSourceDocument}>Source</SmallGhostButton>
-                <History {...props}/>
-                {!overriden ? <Check/> : <Manual/>}
+                <History {...props} />
+                {item.subtotal ? <Nothing /> : !overriden ? <Check /> : <Manual />}
             </div>
         </div>
     )
