c152139876534779d6bb75c296a51b5997bf94f1
Erfang Chen <erfangc@gmail.com>
2021-04-11 00:16:07 -0400

    wip

--- components/Pages/ControlPanel/StockAnalysisControlPanel/Toolbar/ToolButton.tsx
+++ components/Pages/ControlPanel/StockAnalysisControlPanel/Toolbar/ToolButton.tsx
@@ -9,7 +9,7 @@ interface ToolButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttrib
 export function ToolButton({ label, loading, children, className, ...props }: ToolButtonProps) {
     return (
         <div className="flex flex-col space-y-1 justify-center items-center">
-            <button className={`bg-blueGray-600 rounded-md shadow-lg px-4 py-2 focus:outline-none hover:shadow-none transition ease-linear ${className}`} disabled={loading} {...props}>
+            <button className={`${loading ? 'text-blueGray-500 cursor-not-allowed' : 'text-blueGray-200'} bg-blueGray-600 rounded-md shadow-lg px-4 py-2 focus:outline-none hover:shadow-none transition ease-linear ${className}`} disabled={loading} {...props}>
                 {children}
             </button>
             <span className="text-sm text-blueGray-400">{label}</span>
