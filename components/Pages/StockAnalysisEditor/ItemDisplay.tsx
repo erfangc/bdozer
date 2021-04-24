e0f52c461cab2a0e7c521a558bba3efd62e01b82
Erfang Chen <erfangc@gmail.com>
2021-04-23 22:43:31 -0400

    wip

--- components/Pages/StockAnalysisEditor/ItemDisplay.tsx
+++ components/Pages/StockAnalysisEditor/ItemDisplay.tsx
@@ -30,24 +30,24 @@ export function ItemDisplay(props: Props) {
         const {data: fact} = await factBase.getFact(factId)
         window.open(fact.sourceDocument)
     }
-    const isSubtotal = item.type === ItemTypeEnum.SumOfOtherItems
+    const subtotal = item.type === ItemTypeEnum.SumOfOtherItems
     return (
         <div
             className={
                 `cursor-pointer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded py-2 
-                ${isSubtotal ? '' : 'bg-blueGray-800'} shadow-lg px-4 hover:bg-blueGray-700 transition ease-linear 
+                ${subtotal ? '' : 'bg-blueGray-800'} shadow-lg px-4 hover:bg-blueGray-700 transition ease-linear 
                 ${orphaned ? 'ring-2 ring-rose-700' : null}`
             }
             onClick={() => router.push(`/control-panel/stock-analyses/${id}/items/${item.name}`)}
         >
             <div className="flex items-center overflow-hidden">
-                <span className={isSubtotal ? 'font-extrabold' : ''}>
+                <span className={subtotal ? 'font-extrabold' : ''}>
                     {item.description ?? item.name}
                 </span>
             </div>
             <div className="flex justify-start md:justify-end items-center">
                 <NumberFormat
-                    className={isSubtotal ? 'font-extrabold' : ''}
+                    className={subtotal ? 'font-extrabold' : ''}
                     thousandSeparator
                     decimalScale={0}
                     displayType='text'
@@ -57,7 +57,7 @@ export function ItemDisplay(props: Props) {
             <div className="flex justify-start lg:justify-end items-center space-x-4">
                 <SmallGhostButton onClick={openSourceDocument}>Source</SmallGhostButton>
                 <ItemDisplayDetail {...props} />
-                {isSubtotal && !overridden ? <Nothing/> : !overridden ? <Check/> : <Manual/>}
+                {subtotal && !overridden ? <Nothing/> : !overridden ? <Check/> : <Manual/>}
             </div>
         </div>
     )
