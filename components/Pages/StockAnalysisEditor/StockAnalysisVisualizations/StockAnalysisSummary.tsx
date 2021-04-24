01bcc4efcf22fe1cd59ec537954f2b27651fba32
Erfang Chen <erfangc@gmail.com>
2021-04-23 22:33:39 -0400

    wip

--- components/Pages/StockAnalysisEditor/StockAnalysisVisualizations/StockAnalysisSummary.tsx
+++ components/Pages/StockAnalysisEditor/StockAnalysisVisualizations/StockAnalysisSummary.tsx
@@ -24,38 +24,71 @@ export default function StockAnalysisSummary(props: Props) {
             ?
             component = (
                 <div className="grid grid-flow-row gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
-                    <Money value={currentPrice} label={"Current Price"} running={loading}/>
+                    <Money
+                        value={currentPrice}
+                        label={"Current Price"}
+                        running={loading}
+                    />
                     <Money
                         value={targetPrice}
                         label={"Target Price"}
                         state={targetPrice < currentPrice ? 'danger' : 'good'}
                         running={loading}
                     />
-                    <CardPercent value={stockAnalysis?.model?.terminalGrowthRate} label={"Terminal Growth Rate"}
-                                 running={loading}/>
-                    <CardPercent value={derivedStockAnalytics?.discountRate} label={"Discount Rate"} running={loading}/>
-                    <CardPercent value={derivedStockAnalytics?.revenueCAGR} label={"Revenue CAGR"} running={loading}/>
-                    <Number value={terminalPe} label={"Terminal Year P/E"} running={loading}/>
+                    <CardPercent
+                        value={stockAnalysis?.model?.terminalGrowthRate}
+                        label={"Terminal Growth Rate"}
+                        running={loading}
+                    />
+                    <CardPercent
+                        value={derivedStockAnalytics?.discountRate}
+                        label={"Discount Rate"}
+                        running={loading}
+                    />
+                    <CardPercent
+                        value={derivedStockAnalytics?.revenueCAGR}
+                        label={"Revenue CAGR"}
+                        running={loading}
+                    />
+                    <Number
+                        value={terminalPe}
+                        label={"Terminal Year P/E"}
+                        running={loading}
+                    />
                 </div>
             )
             : null
     } else {
         component = <StockAnalysisCharts stockAnalysis={props.stockAnalysis}/>
-
     }
+
     return (
         <div>
             <div className="flex mb-4">
-                <button
-                    className={`text-blueGray-200 text-sm px-2 w-32 py-1.5 transition ease-out rounded-l-xl ${tab === 'cards' ? 'bg-blueGray-700' : 'bg-blueGray-800'} focus:outline-none`}
-                    onClick={() => setTab('cards')}>Cards
-                </button>
-                <button
-                    className={`text-blueGray-200 text-sm px-2 w-32 py-1.5 transition ease-out rounded-r-xl ${tab === 'chart' ? 'bg-blueGray-700' : 'bg-blueGray-800'} focus:outline-none`}
-                    onClick={() => setTab('chart')}>Chart
-                </button>
+                <Button onClick={() => setTab('cards')} active={tab === 'cards'} className="rounded-l-xl">
+                    Cards
+                </Button>
+                <Button onClick={() => setTab('chart')} active={tab === 'chart'} className="rounded-r-xl">
+                    Chart
+                </Button>
             </div>
             {component}
         </div>
+    )
+}
+
+function Button({
+                    active,
+                    children,
+                    className,
+                    ...props
+                }: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & { active: boolean }) {
+    return (
+        <button
+            className={`text-blueGray-200 text-sm px-2 w-32 py-1.5 transition ease-out ${active ? 'bg-blueGray-700' : 'bg-blueGray-800'} hover:bg-blueGray-700 focus:outline-none ${className}`}
+            {...props}
+        >
+            {children}
+        </button>
     )
 }
