2488340f5e12b190b0da0b39b7859bc6d93304f5
Erfang Chen <erfangc@gmail.com>
2021-04-26 21:56:13 -0400

    wip

--- components/Pages/Browse/Browse.tsx
+++ components/Pages/Browse/Browse.tsx
@@ -11,8 +11,14 @@ export function Browse() {
     const stockAnalysisPublication = usePublishedStockAnalysis()
     const [stockAnalyses, setStockAnalyses] = useState<StockAnalysisProjection[]>([])
     const [loading, setLoading] = useState(false)
+    const [underValuedFilter, setUndervaluedFilter] = useState(false)
 
-    async function init() {
+    function toggleUnderValued() {
+        const underValuedOnly = !underValuedFilter
+        setUndervaluedFilter(underValuedOnly);
+    }
+
+    async function refresh() {
         setLoading(true)
         const {data: resp} = await stockAnalysisPublication.findPublishedStockAnalyses(
             undefined,
@@ -27,7 +33,7 @@ export function Browse() {
     }
 
     useEffect(() => {
-        init()
+        refresh()
     }, [])
 
     const router = useRouter()
@@ -36,13 +42,14 @@ export function Browse() {
         router.push(`/${cik}/narrative2`)
     }
 
+    const filteredStockAnalyses = underValuedFilter ? stockAnalyses.filter(sa => sa.currentPrice < sa.targetPrice) : stockAnalyses;
     return (
         <main className="min-h-screen mx-auto container px-2 flex flex-col justify-between max-w-prose">
             <section>
                 <StockAnalysisSearch onSubmit={({cik}) => navigate(cik)} className="mb-20 mt-16"/>
                 <div className="mb-8">
-                    <div className="flex space-x-4">
-                        <StockTab active>#featured stocks</StockTab>
+                    <div className="flex space-x-4 items-center">
+                        <b>Filters:</b> <StockTab active={underValuedFilter} onClick={toggleUnderValued}>undervalued stocks</StockTab>
                     </div>
                 </div>
                 <blockquote className="my-4 pl-6 border-l-4 bg-blueGray-800 py-2 text-sm text-blueGray-300">
@@ -51,7 +58,7 @@ export function Browse() {
                 <div className="flex flex-col space-y-6">
                     {loading ? <LoadingSkeletons/> : null}
                     {
-                        stockAnalyses
+                        filteredStockAnalyses
                             .map(stockAnalysis => (
                                 <StockAnalysisCard
                                     key={stockAnalysis['_id']}
@@ -73,7 +80,7 @@ interface StockTabProps extends React.DetailedHTMLProps<React.ButtonHTMLAttribut
 function StockTab({active, className, children, ...props}: StockTabProps) {
     return (
         <button
-            className={`focus:outline-none px-2 py-1 rounded font-extrabold uppercase transition ${active ? 'bg-blue-500 cursor-default' : 'border-blueGray-500 bg-blueGray-700 shadow-lg hover:bg-blue-500 hover:border-0'} ease-linear ${className}`}
+            className={`focus:outline-none px-2 py-1 rounded font-extrabold uppercase transition ${active ? 'bg-blue-500' : 'border-blueGray-500 bg-blueGray-700 shadow-lg hover:bg-blue-500 hover:border-0'} ease-linear ${className}`}
             {...props}
         >
             {children}
