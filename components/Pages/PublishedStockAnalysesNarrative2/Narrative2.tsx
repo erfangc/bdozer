b24bde44ece6f35d4be5184cba9b11671cb06f92
Erfang Chen <erfangc@gmail.com>
2021-04-27 13:30:38 -0400

    wip

--- components/Pages/PublishedStockAnalysesNarrative2/Narrative2.tsx
+++ components/Pages/PublishedStockAnalysesNarrative2/Narrative2.tsx
@@ -20,7 +20,7 @@ export function Narrative2(props: Props) {
     const { stockAnalysis } = props;
     return (
         <main className="px-4 py-20 container mx-auto max-w-prose overflow-x-scroll md:overflow-x-hidden">
-            <Toolbar />
+            <Toolbar stockAnalysis={stockAnalysis}/>
             <div className="flex flex-col space-y-20 py-0 lg:py-20">
                 <Overview result={stockAnalysis} />
                 <ExecutiveSummary result={stockAnalysis} />
--- components/Pages/PublishedStockAnalysesNarrative2/Toolbar.tsx
+++ components/Pages/PublishedStockAnalysesNarrative2/Toolbar.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import {StockAnalysis2} from "../../../client";
 
-export function Toolbar() {
+interface Props {
+    stockAnalysis: StockAnalysis2
+}
+
+export function Toolbar({stockAnalysis}: Props) {
 
     return (
         <div className="shadow bg-blueGray-800 px-2 py-2 flex space-x-6 fixed bottom-0 lg:top-0 lg:bottom-auto left-0 lg:left-20 right-0 justify-center z-10 overflow-x-scroll lg:overflow-x-auto">
@@ -11,7 +16,7 @@ export function Toolbar() {
                 </svg>
                 Browse More
             </ToolButton>
-            <ToolButton href='/early-access'>
+            <ToolButton href={`/control-panel/stock-analyses/${stockAnalysis['_id']}`}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 17V19H9V17H3ZM3 5V7H13V5H3ZM13 21V19H21V17H13V15H11V21H13ZM7 9V11H3V13H7V15H9V9H7ZM21 13V11H11V13H21ZM15 9H17V7H21V5H17V3H15V9Z" fill="#CBD5E1" />
                 </svg>
