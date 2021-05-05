a656f8028e29febac215865eb20ec83ced5daf9f
Erfang Chen <erfangc@gmail.com>
2021-05-05 10:24:55 -0400

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  purge: false,
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
