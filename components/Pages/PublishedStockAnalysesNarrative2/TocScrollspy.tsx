acf827b615d34e63db9d9dbf75e08e698aa41e34
Erfang Chen <erfangc@gmail.com>
2021-05-05 12:31:05 -0400

    wip

--- components/Pages/PublishedStockAnalysesNarrative2/TocScrollspy.tsx
+++ components/Pages/PublishedStockAnalysesNarrative2/TocScrollspy.tsx
@@ -23,7 +23,7 @@ export function TocScrollspy({stockAnalysis}: Props) {
         setHref(window.location.href)
     }, [])
     return (
-        <nav className="hidden lg:block fixed top-16 w-64 -ml-64 py-4 text-blueGray-200 z-10 bg-blueGray-800 shadow-md rounded">
+        <nav className="hidden lg:block fixed top-12 w-64 right-0 py-4 text-blueGray-200 z-10 bg-blueGray-800 shadow-md rounded text-sm">
             <Scrollspy
                 items={['overview', 'summary', 'business-breakdown', 'future-earnings-per-share', 'terminal-value-calculation', 'fair-value-derivation']}
                 currentClassName={'bg-blue-500 font-bold'}
@@ -49,7 +49,7 @@ export function TocScrollspy({stockAnalysis}: Props) {
             </Scrollspy>
             <div className="border-t border-blueGray-500 pt-4 mt-4">
                 <Link href={`/control-panel/stock-analyses/${stockAnalysis['_id']}`}>
-                    <a className="hover:bg-blue-500 transition ease-in px-4 py-1 rounded border mx-2 border-blueGray-600 block text-sm flex justify-center items-center">
+                    <a className="hover:bg-blue-500 transition ease-in px-4 py-1 rounded border mx-2 border-blueGray-600 block flex justify-center items-center">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3 17V19H9V17H3ZM3 5V7H13V5H3ZM13 21V19H21V17H13V15H11V21H13ZM7 9V11H3V13H7V15H9V9H7ZM21 13V11H11V13H21ZM15 9H17V7H21V5H17V3H15V9Z" fill="#CBD5E1" />
                         </svg>
