f2f9e339545b79ee47950c4bc366bb423ff1518d
Erfang Chen <erfangc@gmail.com>
2021-05-09 14:16:57 -0400

    wip

--- components/Pages/ControlPanel/PublishedToggle.tsx
+++ components/Pages/ControlPanel/PublishedToggle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 interface Props {
-    published?: boolean
+    published?: boolean | '' | 'false' | 'true'
     setPublished: (published?: boolean) => void
 }
 
@@ -24,17 +24,17 @@ export function PublishedToggle({setPublished, published}: Props) {
         <div>
             <button
                 onClick={viewAll}
-                className={`focus:outline-none border-l border-t border-b border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 rounded-l ${published === undefined ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
+                className={`focus:outline-none border-l border-t border-b border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 rounded-l ${published == undefined || published === '' ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
                 All
             </button>
             <button
                 onClick={viewUnpublished}
-                className={`focus:outline-none border-t border-b border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 ${published === false ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
+                className={`focus:outline-none border-t border-b border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 ${published == 'false' ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
                 Unpublished
             </button>
             <button
                 onClick={viewPublished}
-                className={`focus:outline-none border border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 rounded-r self-start md:self-end ${published === true ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
+                className={`focus:outline-none border border-amber-700 transition ease-linear hover:bg-amber-500 hover:text-blueGray-800 px-2 py-1 rounded-r self-start md:self-end ${published == 'true' ? 'bg-amber-500 text-blueGray-800' : 'text-amber-600'}`}>
                 Published
             </button>
         </div>
