cdf3fbdd4f5750fb1c57c8ecbae389fd3a82c156
Erfang Chen <erfangc@gmail.com>
2021-05-20 18:09:40 -0400

    wip

--- components/KPIVisualizer/CompanyKPIsStateManager.ts
+++ components/KPIVisualizer/CompanyKPIsStateManager.ts
@@ -40,6 +40,14 @@ export class CompanyKPIsStateManager {
         };
     }
 
+    hasChildren = (item: Item) => {
+        return (
+            (item.type === ItemTypeEnum.ProductOfOtherItems && item.productOfOtherItems)
+            ||
+            (item.type === ItemTypeEnum.SumOfOtherItems && item.sumOfOtherItems)
+        );
+    }
+
     handleItemEdit = (
         newKPI: KPIMetadata, newItem: Item,
         kpi?: KPIMetadata, item?: Item,
