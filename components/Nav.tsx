73136f8ca05620995b5f476111b6767c20bd22ba
Erfang Chen <erfangc@gmail.com>
2021-08-09 19:02:10 -0400

    added Hamburger menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,19 @@
 import {Logo} from "./Pages/Home/Logo";
 import {PrimaryButton} from "./Common/PrimaryButton";
-import React from "react";
+import React, {useState} from "react";
 import {useAuth0} from "@auth0/auth0-react";
 import Link from 'next/link';
 
 export function Nav() {
+    return (
+        <>
+            <Desktop/>
+            <Mobile/>
+        </>
+    )
+}
+
+export function Desktop() {
 
     const {isAuthenticated, loginWithRedirect, logout} = useAuth0()
 
@@ -17,7 +26,7 @@ export function Nav() {
     }
 
     return (
-        <nav className="bg-chili-100 h-20 text-white items-center flex justify-between antialiased">
+        <nav className="hidden lg:flex bg-chili-100 h-20 text-white items-center justify-between">
             <div className="flex space-x-16">
                 <Logo/>
                 {
@@ -41,4 +50,54 @@ export function Nav() {
             }
         </nav>
     );
+}
+
+function Mobile() {
+
+    const {isAuthenticated, loginWithRedirect, logout} = useAuth0()
+
+    const [active, setActive] = useState(false)
+
+    function login() {
+        loginWithRedirect();
+    }
+
+    function signup() {
+        loginWithRedirect({screen_hint: 'signup'});
+    }
+
+    return (
+        <nav className="lg:hidden bg-chili-100 h-20 text-white items-center flex justify-between antialiased relative">
+            <Logo/>
+            <MenuIcon onClick={() => setActive(!active)}/>
+            <ul className={`absolute w-screen bg-avocado-100 transition duration-500 z-10 top-20 ${active ? 'max-h-screen' : 'max-h-0'} overflow-hidden flex flex-col items-center justify-center`}>
+                <li className="py-4 w-full text-center"><Link href='/watch-list'>Watch List</Link></li>
+                <li className="py-4 w-full text-center"><Link href='/search'>Stocks</Link></li>
+                {
+                    !isAuthenticated ?
+                        <>
+                            <li className="py-4 w-full text-center" onClick={signup}>Sign Up</li>
+                            <li className="py-4 w-full text-center" onClick={login}>Login</li>
+                        </>
+                    : <li className="py-4 w-full text-center" onClick={() => logout()}>Logout</li>
+                }
+            </ul>
+
+        </nav>
+    );
+}
+
+interface Props {
+    onClick: () => void
+}
+
+function MenuIcon({onClick}:Props) {
+    return (
+        <button className="pr-4" onClick={onClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="32px" width="32px" viewBox="0 0 24 24" className="fill-current">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+            </svg>
+        </button>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,109 +1,111 @@
 module.exports = {
-  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  purge: false,
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      spacing: {
-        card: '687px',
-        mobileCard: '382px',
-      },
+    // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: false,
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        transitionProperty: {
+            'height': 'height'
+        },
+        extend: {
+            spacing: {
+                card: '687px',
+                mobileCard: '382px',
+            },
+        },
+        fontFamily: {
+            'sans': ['Poppins', 'sans-serif', 'system-ui'],
+            'mono': ['"JetBrains Mono"', 'ui-monospace']
+        },
+        fontWeight: {
+            thin: 400,
+            normal: 500,
+            bold: 700,
+        },
+        fontSize: {
+            'display': '4.5rem',
+            '5xl': '3.5rem',
+            '4xl': '2.75rem',
+            '3xl': '2.5rem',
+            '2xl': '2.0rem',
+            'xl': '1.5rem',
+            'lg': '1.25rem',
+            'base': '1rem',
+            'sm': '0.875rem',
+            'xs': '0.625rem',
+            'xxs': '0.5rem',
+        },
+        lineHeight: {
+            8: '91px',
+            7: '64px',
+            6: '52px',
+            5: '48px',
+            4: '40px',
+            3: '32px',
+            2: '24px',
+            1: '16px',
+        },
+        colors: {
+            white: '#fff',
+            lightGreen: {
+                100: '#ECF6E3',
+                75: '#F1F8EA',
+                50: '#F5FAF1',
+                25: '#FAFDF8',
+            },
+            lime: {
+                100: '#B4DD62',
+                75: '#C7E589',
+                50: '#D9EEB0',
+                25: '#ECF6D8',
+            },
+            avocado: {
+                100: '#6F9421',
+                75: '#93AF58',
+                50: '#B7C990',
+                25: '#DBE4C7',
+            },
+            chili: {
+                100: '#253209',
+                75: '#5B6546',
+                50: '#929884',
+                25: '#C8CCC1',
+            },
+            deepNavy: {
+                100: '#081827',
+            },
+            navy: {
+                100: '#0B1E33',
+                75: '#485666',
+                50: '#858E99',
+                25: '#C2C7CC',
+            },
+            dashboardGray: {
+                100: '#36424C',
+                75: '#687179',
+                50: '#9AA0A5',
+                25: '#CDD0D2',
+            },
+            red: {
+                100: '#FB544C',
+                75: '#FC7F79',
+                50: '#FDA9A5',
+                25: '#FED4D2',
+            },
+            gray: {
+                1: '#FAFCFA',
+                2: '#D8DDD8',
+                3: '#C8CDC8',
+                4: '#B6BCB5',
+                5: '#939892',
+                6: '#6F766E',
+                7: '#505950',
+                8: '#32372B',
+                9: '#1F2824',
+            }
+        },
     },
-    fontFamily: {
-      'sans': ['Poppins', 'sans-serif', 'system-ui'],
-      'mono': ['"JetBrains Mono"', 'ui-monospace']
+    variants: {
+        extend: {},
     },
-    fontWeight: {
-      thin: 400,
-      normal: 500,
-      bold: 700,
-    },
-    fontSize: {
-      'display': '4.5rem',
-      '5xl': '3.5rem',
-      '4xl': '2.75rem',
-      '3xl': '2.5rem',
-      '2xl': '2.0rem',
-      'xl': '1.5rem',
-      'lg': '1.25rem',
-      'base': '1rem',
-      'sm': '0.875rem',
-      'xs': '0.625rem',
-      'xxs': '0.5rem',
-    },
-    lineHeight: {
-      8: '91px',
-      7: '64px',
-      6: '52px',
-      5: '48px',
-      4: '40px',
-      3: '32px',
-      2: '24px',
-      1: '16px',
-    },
-    colors: {
-      white: '#fff',
-      lightGreen: {
-        100: '#ECF6E3',
-        75: '#F1F8EA',
-        50: '#F5FAF1',
-        25: '#FAFDF8',
-      },
-      lime: {
-        100: '#B4DD62',
-        75: '#C7E589',
-        50: '#D9EEB0',
-        25: '#ECF6D8',
-      },
-      avocado: {
-        100: '#6F9421',
-        75: '#93AF58',
-        50: '#B7C990',
-        25: '#DBE4C7',
-      },
-      chili: {
-        100: '#253209',
-        75: '#5B6546',
-        50: '#929884',
-        25: '#C8CCC1',
-      },
-      deepNavy: {
-        100: '#081827',
-      },
-      navy: {
-        100: '#0B1E33',
-        75: '#485666',
-        50: '#858E99',
-        25: '#C2C7CC',
-      },
-      dashboardGray: {
-        100: '#36424C',
-        75: '#687179',
-        50: '#9AA0A5',
-        25: '#CDD0D2',
-      },
-      red: {
-        100: '#FB544C',
-        75: '#FC7F79',
-        50: '#FDA9A5',
-        25: '#FED4D2',
-      },
-      gray: {
-        1: '#FAFCFA',
-        2: '#D8DDD8',
-        3: '#C8CDC8',
-        4: '#B6BCB5',
-        5: '#939892',
-        6: '#6F766E',
-        7: '#505950',
-        8: '#32372B',
-        9: '#1F2824',
-      }
-    },
-  },
-  variants: {
-    extend: {
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
