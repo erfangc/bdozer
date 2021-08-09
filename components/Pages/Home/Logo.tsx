815217b195bb9c2a56f5f7b709eaa67156bc4e1d
Erfang Chen <erfangc@gmail.com>
2021-08-08 21:50:15 -0400

    pushing new logo

--- components/Pages/Home/Logo.tsx
+++ components/Pages/Home/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Logo() {
     return (
         <a className="font-bold pl-6 lg:pl-12 flex items-center space-x-2" href="/">
-            <div className="h-4 w-4 bg-lime-100"/>
+            <img src="./logo.svg" alt=""/>
             <span style={{fontSize: '26px'}} className="font-bold">Bdozer</span>
         </a>
     )
